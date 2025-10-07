8cf06e0b94ec008fd74f40e2be2955e9c139d7d3
Wudafk <wudafk@gmail.com>
2025-10-07 19:11:45 +0700

    3

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,7 @@ import withNuxt from './.nuxt/eslint.config.mjs'
 export default withNuxt({
   // Your custom configs here
   "rules": {
-    "vue/block-tag-newline": "off" // Полностью отключает правило
+    "vue/block-tag-newline": "off", // Полностью отключает правило
+    "@stylistic/semi": "off"
   }
 })
